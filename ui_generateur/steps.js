2f8faebeb7c0dfea5a9b333fae5df40d70a38b4c
TriNGUYEN-Orange <caothientri.nguyen@orange.com>
2026-06-03 10:30:03 +0200

    update manuel

--- ui_generateur/steps.js
+++ ui_generateur/steps.js
@@ -30,7 +30,7 @@ const MIGRATION_CONTENT = {
         titre: "Étape 4 : Restauration magique ✨",
         desc: `
             <ol style="margin-top: 10px; padding-left: 20px; line-height: 1.6;">
-                📝 <b>Note :</b> Cet outil fonctionne sur <b>Livebox 6</b> (Livebox 7 est en cours de développement)
+                📝 <b>Note :</b> Cet outil fonctionne sur <b>Livebox 6 / W7</b>
                 <li>Allez sur la nouvelle interface : <a href="http://192.168.1.1" target="_blank" style="color: #ff7900; font-weight: bold;">http://192.168.1.1</a> et connectez-vous.</li>
                 <li>Cliquez sur votre favori <b>🚀 Application</b>.</li>
                 <li>Détendez-vous pendant que tous vos paramètres sont restaurés automatiquement !</li>
