8459e6ff0743566a68ed96d4d02c458ae3c5d7f6
TriNGUYEN-Orange <caothientri.nguyen@orange.com>
2026-06-11 16:43:01 +0200

    update bug url wtf

--- push/box7/push_main.js
+++ push/box7/push_main.js
@@ -27,7 +27,9 @@
     };
 
     // const BASE_URL = detectBaseUrl();
-    const BASE_URL = "http://127.0.0.1:5500/";
+    //const BASE_URL = "http://127.0.0.1:5500/";
+    const BASE_URL = "https://tringuyen-orange.github.io/Outil-Migration-LiveBox-Pro/";
+
 
     const LISTE_MODULES = [
         { actif: false, nomUI: "Réveil du système", nomEnv: "Wake-Up", fichier: "push_wakeup.js", fonction: "executerWakeUp" },
