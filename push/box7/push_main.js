f39c7c7f1cddfe3cd1c818724036eb1fa55e641e
Jeune Albert <martinconan@gmail.com>
2026-09-01 14:37:06 +0200

    Update push_main.js
    Changement lien dans const BASE_URL

--- push/box7/push_main.js
+++ push/box7/push_main.js
@@ -28,7 +28,7 @@
 
     // const BASE_URL = detectBaseUrl();
     //const BASE_URL = "http://127.0.0.1:5500/";
-    const BASE_URL = detectBaseUrl() || "https://tringuyen-orange.github.io/Outil-Migration-LiveBox-Pro";
+    const BASE_URL = detectBaseUrl() || "https://jeune-albert.github.io/Outil-Migration-LiveBox-Pro";
 
 
 
